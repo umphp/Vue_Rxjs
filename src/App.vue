a03c0ffe91ae39abe14c56bbe83cffa17f67010e
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-07-11 14:01:23 +0800

    create rxjs streams in subscriptions

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h1>{{interval$}}</h1>
+    <h1>{{timesTwo$}}</h1>
+    <h1>{{timesThree$}}</h1>
+  </section>
+</template>
+
+<script>
+import { Observable } from "rxjs"
+
+export default {
+  subscriptions() {
+    const interval$ = Observable.interval(1000);
+    const timesTwo$ = interval$.map(i => i * 2);
+    const timesThree$ = interval$.map(i => i * 3);
+    return {
+      interval$,
+      timesTwo$,
+      timesThree$
+    }
+  }
+}
+</script>
+
